73332aa4a3d142af292a6aa1101997a9e911c291
VJustice <Filipe@cenas.com>
2015-12-01 17:52:37 +0000

    I´m here

--- pa.iscde.deepsearch/src/pa/iscde/deepsearch/ASTVisitor_deepSearch.java
+++ pa.iscde.deepsearch/src/pa/iscde/deepsearch/ASTVisitor_deepSearch.java
@@ -51,7 +51,7 @@ public class ASTVisitor_deepSearch extends ASTVisitor {
 						search_result);
 			}
 		}
-		System.out.println("just test 2");
+		System.out.println("just test 3");
 		return true;
 		
 	}
