63c78e1813430b4364b560ae1ac2091e6a3b9898
VJustice <Filipe@cenas.com>
2015-12-04 16:24:32 +0000

    Added Test Composite to test new Auto_complete found out is Jorge's work

--- pa.iscde.deepsearch/src/api/IAdvancedSearch.java
+++ pa.iscde.deepsearch/src/api/IAdvancedSearch.java
@@ -2,6 +2,31 @@ package api;
 
 public interface IAdvancedSearch {
 
+	/**
+	 * Invoked to add new Combo options to search.
+	 * @param option_name to insert on existing Combo
+	 */
+	public void addComboElement(String option_name);
+	
+	/**
+	 * Invoked to add array of elements to combo instead of only one.
+	 * @param combo_elements String[] array of elements
+	 */
+	public void addArrayElementsToCombo(String[] combo_elements);
+	
+	/**
+	 * Invoked to get an element from Combo.
+	 * @param index of Combo Element
+	 * @return String Element of Combo
+	 */
+	public String getComboElement(int index);
+
+	/**
+	 * Invoked to get all elements from Combo.
+	 * @return String[] all Combo Elements
+	 */
+	public String[] getComboElements();
+	
 	
 	
 }
