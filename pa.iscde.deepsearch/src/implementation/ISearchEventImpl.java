665c0569d967cb7599a8b114203bd191d916dcbe
VJustice <Filipe@cenas.com>
2015-12-07 18:20:15 +0000

    my stash

--- pa.iscde.deepsearch/src/implementation/ISearchEventImpl.java
+++ pa.iscde.deepsearch/src/implementation/ISearchEventImpl.java
@@ -4,9 +4,6 @@ import api.ISearchEvent;
 
 public class ISearchEventImpl implements ISearchEvent {
 
-	public ISearchEventImpl() {
-	}
-
 	@Override
 	public void getSearchedParameters(String searched, String combo_element, String specific_element) {
 	}
