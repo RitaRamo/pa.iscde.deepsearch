ea656a63f506dbff61dbe944cd32e4a96ab4d2a4
VJustice <Filipe@cenas.com>
2015-12-07 18:21:32 +0000

    LOL

--- pa.iscde.deepsearch/src/api/ISearchEvent.java
+++ pa.iscde.deepsearch/src/api/ISearchEvent.java
@@ -3,6 +3,5 @@ package api;
 public interface ISearchEvent {
 
 	public void getSearchedParameters(String searched, String combo_element, String specific_element);
-	//Todas as extensoes vao ser notificadas que esta procura foi feita
 	
 }
